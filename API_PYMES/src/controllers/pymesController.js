8d296093e9ef123eed1ee260fa2912f7785d644b
Bastiancito <bastian.camusj@usm.cl>
2024-05-09 22:39:39 -0400

    eo

--- API_PYMES/src/controllers/pymesController.js
+++ API_PYMES/src/controllers/pymesController.js
@@ -2,7 +2,7 @@ const database = require('../db');
 
 
 const createTablePymes = (req, res) => {
-  const query =  'CREATE TABLE IF NOT EXISTS pymes(id_pyme int AUTO_INCREMENT, nombre_pyme VARCHAR(50),nombre_propietario VARCHAR(50),doc_identidad VARCHAR(50), direccion_pyme VARCHAR(50), direccion_contacto VARCHAR(50),descripcion_pyme VARCHAR(128), PRIMARY KEY(id_pyme))';
+  const query =  'CREATE TABLE IF NOT EXISTS pymes(id_pyme int AUTO_INCREMENT, nombre_pyme VARCHAR(50),nombre_propietario VARCHAR(50),doc_identidad VARCHAR(50), direccion_pyme VARCHAR(50), direccion_contacto VARCHAR(50),descripcion_pyme VARCHAR(128),etiquetas_pyme VARCHAR(128), PRIMARY KEY(id_pyme))';
 
   database.query(query, (err) => {
       if (err) throw err;
@@ -26,9 +26,9 @@ const getPymes = (req, res) => {
 
   const createPyme = (req, res) => {
     const { nombre_pyme, nombre_propietario, doc_identidad, direccion_pyme, direccion_contacto, descripcion_pyme } = req.body;
-    const query = 'INSERT INTO pymes (nombre_pyme, nombre_propietario, doc_identidad, direccion_pyme, direccion_contacto, descripcion_pyme) VALUES (?, ?, ?, ?, ?, ?)';
+    const query = 'INSERT INTO pymes (nombre_pyme, nombre_propietario, doc_identidad, direccion_pyme,etiquetas_pyme, direccion_contacto, descripcion_pyme) VALUES (?, ?, ?, ?, ?, ?)';
   
-    database.query(query, [nombre_pyme, nombre_propietario, doc_identidad, direccion_pyme, direccion_contacto, descripcion_pyme], (err, results) => {
+    database.query(query, [nombre_pyme, nombre_propietario, doc_identidad, direccion_pyme,etiquetas_pyme ,direccion_contacto, descripcion_pyme], (err, results) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ error: 'Error al crear pyme', details: err.message });
